a54583b10078f3b211e458760cffe8c06c7e0a9a
TahirAhmad <tahirahmad0234@gmail.com>
2024-10-15 23:41:51 +0600

    fix search with filter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,33 +5,56 @@ import Pagination from "../components/Pagination";
 
 const HomePage = ({ wishlist, onWishlistToggle }) => {
   const [books, setBooks] = useState([]);
+  const [filteredBooks, setFilteredBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState("");
 
   useEffect(() => {
     const loadBooks = async () => {
       setLoading(true);
-      const data = await fetchBooks(searchTerm, currentPage);
-      const filteredBooks = data.results.filter(
-        (book) =>
-          book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          book.subjects.some((subject) =>
-            subject.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-      );
-      setBooks(filteredBooks);
-      setTotalPages(Math.ceil(data.count / 32));
+      const data = await fetchBooks();
+      setBooks(data.results);
       setLoading(false);
     };
     loadBooks();
-  }, [searchTerm, currentPage]);
+  }, []);
+
+  useEffect(() => {
+    filterBooks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm, selectedGenre, books, currentPage]);
+
+  const filterBooks = () => {
+    const searchTermLower = searchTerm.toLowerCase();
+    const filtered = books.filter(
+      (book) =>
+        (book.title.toLowerCase().includes(searchTermLower) ||
+          book.subjects.some((subject) =>
+            subject.toLowerCase().includes(searchTermLower)
+          )) &&
+        (selectedGenre ? book.subjects.includes(selectedGenre) : true)
+    );
+
+    setTotalPages(Math.ceil(filtered.length / 32)); // Calculate total pages based on the filtered results
+    setFilteredBooks(filtered.slice((currentPage - 1) * 32, currentPage * 32));
+  };
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
     setCurrentPage(1);
   };
+
+  const handleGenreChange = (e) => {
+    setSelectedGenre(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const uniqueGenres = [
+    ...new Set(books.flatMap((book) => book.subjects)),
+  ].sort();
 
   return (
     <div>
@@ -42,6 +65,14 @@ const HomePage = ({ wishlist, onWishlistToggle }) => {
           value={searchTerm}
           onChange={handleSearchChange}
         />
+        <select value={selectedGenre} onChange={handleGenreChange}>
+          <option value="">All Genres</option>
+          {uniqueGenres.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
       </div>
 
       <div className="grid grid-cols-1 gap-10 md:grid-cols-[220px_1fr] md:gap-5 lg:grid-cols-[290px_1fr] lg:gap-10 w-full">
@@ -50,7 +81,7 @@ const HomePage = ({ wishlist, onWishlistToggle }) => {
           {loading ? (
             <p>Loading...</p>
           ) : (
-            books.map((book) => (
+            filteredBooks.map((book) => (
               <BookCard
                 book={book}
                 key={book.id}
@@ -60,12 +91,12 @@ const HomePage = ({ wishlist, onWishlistToggle }) => {
             ))
           )}
         </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
       </div>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={setCurrentPage}
-      />
     </div>
   );
 };
