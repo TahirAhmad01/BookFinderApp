3c026a6a36e00dbac01db8ee7c51431c33c28373
TahirAhmad <tahirahmad0234@gmail.com>
2024-11-01 21:53:36 +0600

    fix dark mode for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,56 +15,34 @@ function NavBar() {
     setIsOpen(!isOpen);
   };
 
-  const closeDrawer = () => {
+  const handleLinkClick = () => {
     setIsOpen(false);
   };
 
   return (
-    <nav className="bg-white border-gray-50 dark:bg-gray-900 shadow-md sticky top-0 w-full z-50">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center space-x-3">
-          <div className="h-10 w-10 flex justify-center items-center bg-gray-200 rounded-full dark:bg-gray-700">
-            <img src={logo} className="h-6" alt="Logo" />
-          </div>
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Book Finder
-          </span>
-        </Link>
-
-        <div className="md:hidden flex items-center justify-end">
-          <button
-            onClick={toggleDrawer}
-            className="p-2 text-gray-700 dark:text-gray-300"
-            aria-label="Toggle menu"
+    <React.Fragment>
+      <nav className="md:backdrop-blur-xl bg-[#f1f5f9]/20 dark:bg-[#0b1327]/20 border-gray-50 dark:bg-gray-900 shadow-md sticky top-0 left-0 right-0 w-full z-50 border-b dark:border-gray-800">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+            <div className="h-10 w-10 flex justify-center items-center bg-gray-200 rounded-full">
+              <img src={logo} className="h-6" alt="Logo" />
+            </div>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Book Finder
+            </span>
+          </Link>
+          <div className="flex items-center md:hidden">
+            <ThemeToggle />
+            <button
+              onClick={toggleDrawer}
+              className="p-2 text-gray-700 dark:text-gray-300"
+              aria-label="Open drawer"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
-          <ThemeToggle />
-        </div>
-
-        <div
-          className={`fixed top-0 right-0 w-3/4 h-full bg-white shadow-lg z-50 transform transition-transform dark:bg-gray-800 ${
-            isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-        >
-          <div className="flex justify-between py-5 px-4 border-b dark:border-gray-700">
-            <h2 className="text-2xl font-semibold dark:text-white">Menu</h2>
-            <button onClick={toggleDrawer} aria-label="Close drawer">
               <svg
-                className="w-6 h-6 dark:text-gray-300"
+                className="w-6 h-6"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -74,43 +52,98 @@ function NavBar() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
+                  d="M4 6h16M4 12h16m-7 6h7"
                 />
               </svg>
             </button>
           </div>
-          <ul className="flex flex-col p-4">
-            {navLinks.map((item) => (
-              <li key={item.name}>
-                <Link
-                  to={item.link}
-                  className="block py-2 text-gray-900 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
-                  onClick={closeDrawer}
+          <div
+            className={`md:hidden fixed top-0 right-0 w-3/4 h-full bg-white dark:bg-gray-800 dark:text-white shadow-lg z-50 transform transition-transform max-w-[350px]  ${
+              isOpen ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <div className="flex justify-between py-5 px-4 border-b">
+              <h2 className="text-2xl font-semibold">Menu</h2>
+              <button onClick={toggleDrawer} aria-label="Close drawer">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="hidden md:flex items-center justify-center md:space-x-3">
-          <ul className="flex space-x-3">
-            {navLinks.map((item) => (
-              <li key={item.name}>
-                <Link
-                  to={item.link}
-                  className="block py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700"
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <ul className="flex flex-col py-4 px-2">
+              {navLinks.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    to={item.link}
+                    className="block p-2 text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-white rounded-md"
+                    onClick={() =>
+                      handleLinkClick(
+                        `/path/to/${item.name
+                          .toLowerCase()
+                          .replace(" ", "_")}.json`
+                      )
+                    }
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+            <div className="relative mt-3 md:hidden">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
                 >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <ThemeToggle />
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="search-navbar"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search..."
+              />
+            </div>
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:border-gray-700 mr-3">
+              {navLinks.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    to={item.link}
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <ThemeToggle />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </React.Fragment>
   );
 }
 
